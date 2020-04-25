d23365a088309ff246c6e3b364adc699f8efe01d
Benjamin Albrecht <ben@4bai.de>
2020-04-25 14:06:57 +0200

    basic styles setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './index.scss'
+import './styles/index.scss'
 
 import Virus from './components/Virus'
 import Cell from './components/Cell'
@@ -10,13 +10,6 @@ const covid19 = new Virus('covid19');
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-
-      <div className="nav">
-        <button onClick={covid19.move}>Move 1</button>
-        <button onClick={covid19.moveTo} >Move 2</button>
-        <button onClick={covid19.moveSpline} >Move Spline</button>
-      </div>
       <div className="stage">
         <div className="scene">
           {covid19.render()}
@@ -24,7 +17,19 @@ function App() {
           <Cell />
         </div>
       </div>
+      <div className="sidebar">
+      <h1 className="logo">VIRUSIM</h1>
+        <div className="info-box">
+          <h4 className="subtitle">What is an Infection</h4>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
       <Timeline />
+      <div className="nav">
+        <button onClick={covid19.move}>Move 1</button>
+        <button onClick={covid19.moveTo} >Move 2</button>
+        <button onClick={covid19.moveSpline} >Move Spline</button>
+      </div>
     </div>
   )
 }
