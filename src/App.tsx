c6961819e764451aad17379f616660e21d4aed30
Benjamin Albrecht <ben@4bai.de>
2020-04-25 03:15:43 +0200

    simple virus animation test, add anime js

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './index.scss'
 
+import Virus from './components/Virus'
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <div className="stage">
+        <Virus></Virus>
+      </div>
     </div>
   )
 }
