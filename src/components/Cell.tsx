07cbde0c946fada285de0269694ed125d40b1c99
dgrusemann <dominik.grusemann@gmail.com>
2020-04-25 10:09:11 +0200

    reenable controls

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -8,25 +8,23 @@ class Cell extends Component {
 
   render() {
     return (
-      <div className="cell">
-        <div className="virusWrapper" style={{ width: '300px', height: '300px', left: '500px' }}>
-          <svg
-            viewBox={`${-CELL_MEMBRANE_WIDTH} ${-CELL_MEMBRANE_WIDTH} ${CELL_RADIUS * 2 + CELL_MEMBRANE_WIDTH * 2} ${CELL_RADIUS * 2 + CELL_MEMBRANE_WIDTH * 2}`}
-            className="pathTest" version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink">
-            {/*cell membrane*/}
-            <circle fill="#C7DD94" stroke="#98C557" strokeWidth={CELL_MEMBRANE_WIDTH} strokeMiterlimit="10"
-                    cx={CELL_RADIUS}
-                    cy={CELL_RADIUS}
-                    r={CELL_RADIUS}/>
-            {/*nucleus*/}
-            <circle fill="#96C256" stroke="#81B430" strokeWidth="20" strokeMiterlimit="10"
-                    cx={CELL_RADIUS}
-                    cy={CELL_RADIUS}
-                    r={NUCLEUS_RADIUS}/>
-          </svg>
-        </div>
+      <div className="cell" style={{ width: '300px', height: '300px', left: '500px' }}>
+        <svg
+          viewBox={`${-CELL_MEMBRANE_WIDTH} ${-CELL_MEMBRANE_WIDTH} ${CELL_RADIUS * 2 + CELL_MEMBRANE_WIDTH * 2} ${CELL_RADIUS * 2 + CELL_MEMBRANE_WIDTH * 2}`}
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink">
+          {/*cell membrane*/}
+          <circle fill="#C7DD94" stroke="#98C557" strokeWidth={CELL_MEMBRANE_WIDTH} strokeMiterlimit="10"
+                  cx={CELL_RADIUS}
+                  cy={CELL_RADIUS}
+                  r={CELL_RADIUS}/>
+          {/*nucleus*/}
+          <circle fill="#96C256" stroke="#81B430" strokeWidth="20" strokeMiterlimit="10"
+                  cx={CELL_RADIUS}
+                  cy={CELL_RADIUS}
+                  r={NUCLEUS_RADIUS}/>
+        </svg>
       </div>
     )
   }
