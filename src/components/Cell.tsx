927e1f91564662979412df617bf40657e61a0f23
dgrusemann <dominik.grusemann@gmail.com>
2020-04-25 10:21:03 +0200

    scale cell down and make it bigger

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 
-const CELL_MEMBRANE_WIDTH = 20
-const CELL_RADIUS: number = 500
-const NUCLEUS_RADIUS: number = 150
+const CELL_MEMBRANE_WIDTH = 2
+const CELL_RADIUS: number = 50
+const NUCLEUS_RADIUS: number = 15
 
 class Cell extends Component {
 
   render() {
     return (
-      <div className="cell" style={{ width: '300px', height: '300px', left: '500px' }}>
+      <div className="cell" style={{ width: '500px', height: '500px', left: '500px' }}>
         <svg
           viewBox={`${-CELL_MEMBRANE_WIDTH} ${-CELL_MEMBRANE_WIDTH} ${CELL_RADIUS * 2 + CELL_MEMBRANE_WIDTH * 2} ${CELL_RADIUS * 2 + CELL_MEMBRANE_WIDTH * 2}`}
           version="1.1"
@@ -20,7 +20,7 @@ class Cell extends Component {
                   cy={CELL_RADIUS}
                   r={CELL_RADIUS}/>
           {/*nucleus*/}
-          <circle fill="#96C256" stroke="#81B430" strokeWidth="20" strokeMiterlimit="10"
+          <circle fill="#96C256" stroke="#81B430" strokeWidth={CELL_MEMBRANE_WIDTH} strokeMiterlimit="10"
                   cx={CELL_RADIUS}
                   cy={CELL_RADIUS}
                   r={NUCLEUS_RADIUS}/>
