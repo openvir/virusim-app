7c68799990bcc64cd7e1b91eaee06d3c3018cc6e
dgrusemann <dominik.grusemann@gmail.com>
2020-04-26 15:42:57 +0200

    add new description texts

--- src/App.tsx
+++ src/App.tsx
@@ -14,15 +14,16 @@ import { RnaAnimations } from './components/RnaAnimations'
 
 const covid19 = new Virus('covid19')
 const rna = new RnaAnimations()
-const scene = new Scene('');
+const scene = new Scene('')
 
 const keyframes: Array<Keyframe> = [
   {
     seconds: 0,
-    title: 'Description virus',
+    title: 'Virus SARS-CoV-2 –  What is it made of?',
     description:
-      'SARS-CoV-2 is a virus belonging to the family of the Coronaviridae; it is closely related to SARS-CoV and MERS-CoV. \n' +
-      'Let’s have a look at its main components. At its core we find the genetic information stored in form of a single-stranded RNA; to protect the genome, the RNA strand is bound to a protein and both build the helical nucleocapsid. A lipid envelope surrounds the entire nucleocapsid while the matrix proteins connect the outer envelope with the inner nucleocapsid. On the surface of the virus spike-like proteins stick out which are critical for entering and infecting the host cell. \n',
+      'Let’s have a look at the main components of this virus.\n\nAt its core we find the genetic information stored in the form of a single-strand RNA. To protect the genome, the RNA strand is bound to a protein and both build a helical nucleocapsid.\n\n' +
+      'An outer lipid envelope surrounds the inner nucleocapsid; matrix proteins connect the two structures.\n\n' +
+      'On the surface of the virus many crown-like spikes stick out. Those are essential  for entering and infecting the host cell and give the virus family its name: Coronaviridae',
     elements: [
       {
         element: covid19,
@@ -44,14 +45,14 @@ const keyframes: Array<Keyframe> = [
         y: 1,
         rotation: 0,
         scale: 1,
-      },      
+      },
     ],
   },
   {
     seconds: 10,
-    title: 'Description host cell ',
+    title: 'The Target of the Virus - Cells in Our Lungs',
     description:
-      'Once a droplet with viruses has entered our respiratory system, it travels through the bronchi deep down into our lung where the alveoli sit. These are responsible for the exchange of oxygen and carbon dioxide molecules to and from the bloodstream. Cells that make up the alveoli also happen to have a marker on their surface, named ACE-2 or angiotensin-converting enzyme 2.',
+      'Once a droplet with viruses has entered our respiratory system, it travels deep down into our lung where the alveoli sit. These are responsible for the exchange of oxygen and carbon dioxide to and from the bloodstream. Cells that make up the alveoli happen to have a marker on their surface, named ACE-2 or angiotensin-converting enzyme 2.',
     elements: [
       {
         element: covid19,
@@ -73,74 +74,72 @@ const keyframes: Array<Keyframe> = [
         y: 0,
         rotation: 0,
         scale: 1,
-      },      
+      },
     ],
   },
   {
     seconds: 20,
-    title: 'Entry',
+    title: 'Entry Mechanism - A Key and Its Lock',
     description:
       'The ACE-2 marker on the surface of the cells deep down in our lungs acts as a receptor for the spike protein, the characteristic crown-like feature of the SARS-CoV-2 virus. In other words, SARS-CoV-2 carries a key with it that fits perfectly into a lock on our lung cells.',
-      elements: [
-        {
-          element: covid19,
-          x: 376,
-          y: 442,
-          rotation: 156,
-          scale: 1,
-        },
-        {
-          element: rna,
-          x: 376,
-          y: 442,
-          rotation: 156,
-          scale: 1,
-        },
-        {
-          element: scene,
-          x: 340,
-          y: -137,
-          rotation: 0,
-          scale: 2,
-        },      
-      ],
+    elements: [
+      {
+        element: covid19,
+        x: 376,
+        y: 442,
+        rotation: 156,
+        scale: 1,
+      },
+      {
+        element: rna,
+        x: 376,
+        y: 442,
+        rotation: 156,
+        scale: 1,
+      },
+      {
+        element: scene,
+        x: 340,
+        y: -137,
+        rotation: 0,
+        scale: 2,
+      },
+    ],
   },
   {
     seconds: 30,
-    title: 'Fusion',
+    title: 'Fusion of Cell and Virus',
     description:
-      'When the spike protein of the virus binds to the ACE-2 receptor, “a door opens” and the virus can enter the cell. This process is called “fusion” and requires the plasma membrane of the host cell and the viral membrane to merge. The key spike protein is also called a fusion protein and can be found on the surface of many other viruses like HIV, Influenza and Ebola.',
-      elements: [
-
-      ],
+      'When the spike protein of the virus binds to the ACE-2 receptor, “a door opens” and the virus is able to enter the cell. This process is called “fusion” and requires the plasma membrane of the host cell and the viral membrane to merge.',
+    elements: [],
   },
   {
     seconds: 40,
-    title: 'Viral RNA release',
+    title: 'Milestone - Virus Releases its RNA',
     description:
-      'Once fusion of the virus and the host cell has occurred, the viral genome is released directly into the cytoplasms of the host cell. The virus has now reached its first big milestone and can start the process of  multiplication. ',
-      elements: [
-        {
-          element: covid19,
-          x: 354,
-          y: 353,
-          rotation: 236,
-          scale: 1,
-        },
-        {
-          element: rna,
-          x: 354,
-          y: 353,
-          rotation: 236,
-          scale: 1,
-        },
-      ],
+      'Once fusion of the virus and the host cell has occurred, the viral genome is released directly into the cytoplasms of the host cell. The virus has now reached its first big milestone and can start the process of multiplication.',
+    elements: [
+      {
+        element: covid19,
+        x: 354,
+        y: 353,
+        rotation: 236,
+        scale: 1,
+      },
+      {
+        element: rna,
+        x: 354,
+        y: 353,
+        rotation: 236,
+        scale: 1,
+      },
+    ],
   },
   {
-    seconds: 45,
+    seconds: 50,
     title: 'RNA Replication',
     description:
-      'The viral RNA is now in the cytoplasms of the host cell and can start to replicate. Goal is to produce its 27 different structural proteins that are needed to assemble a new virus - not only one but several thousand. To do so, first the genetic information of the virus has to be replicated. In an initial step the virus borrows parts of the host replication machinery until it has made all necessary tools that allows to replicate its genomic information on its own.',
+      'The viral RNA has made it into the cytoplasms of the host cell functioning as the blueprint for making thousands and thousands of more viruses. In an initial step, the virus borrows parts of the host’s replication machinery until it has made all necessary tools that allows it to be independent. Then it starts replicating its genomic information that code for the structural components needed to make new viruses.',
     elements: [
       {
         element: rna,
@@ -160,51 +159,31 @@ const keyframes: Array<Keyframe> = [
     ],
   },
   {
-    seconds: 50,
-    title: 'Production of Helper = Viral Polymerase',
-    description: '7',
-    elements: [],
-  },
-  {
     seconds: 60,
-    title: 'RNA Replication',
-    description: '8',
+    title: 'Translation - From Genetic Code to Actual Protein',
+    description:
+      'Once the genetic information is replicated it has to be translated into the actual structural proteins that make up the virus, like the spike, envelope, matrix and nucleocapsid proteins. This process happens in the cytoplasm or the endoplasmic reticulum of the host cell.',
     elements: [],
   },
   {
     seconds: 70,
-    title: 'Transcription + Translation',
-    description: '9',
+    title: 'Assembly of the New Viruses',
+    description:
+      'New structural viral proteins have been manufactured in the endoplasmic reticulum while new viral RNA and nucleocapsid proteins were made in the cytoplasm of the host. All parts are now assembled and a new mature viron is formed.',
     elements: [],
   },
   {
     seconds: 80,
-    title: 'Viral RNA Packaging in Cytoplasm',
-    description: '10',
-    elements: [],
-  },
-  {
-    seconds: 85,
-    title: 'Structural Protein Synthesis',
-    description: '11',
+    title: 'Exocytosis - Release',
+    description:
+      'In a process called exocytosis, many newly build virons exit the host cell.',
     elements: [],
   },
   {
     seconds: 90,
-    title: 'Budding + Assembly',
-    description: '12',
-    elements: [],
-  },
-  {
-    seconds: 95,
-    title: 'Release',
-    description: '13',
-    elements: [],
-  },
-  {
-    seconds: 100,
     title: 'Invasion',
-    description: '14',
+    description:
+      'Now thousands and thousand of more viruses have been made. They leave through the host’s respiratory system through sneezing or coughing – ready to infect new hosts.',
     elements: [],
   },
 ]
